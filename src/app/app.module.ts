672ae3100edcb3f84f10ccd62a458befe1c2058b
vamsik072 <vamsik072@gmail.com>
2021-04-25 12:31:56 +0530

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { StudentsComponent } from './students/students.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, StudentsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,5 @@
+import { Route } from "@angular/router";
+
+const appRoute: Route = [
+  
+]
